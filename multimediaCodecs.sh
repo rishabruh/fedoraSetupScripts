fd2e37ea5de1bbc256478c093aae38dd0f2aa01b
rishabruh <rishabhrocks2337@gmail.com>
2022-01-14 09:43:41 +0530

    Added multimedia codec script

---
+++ multimediaCodecs.sh
@@ -0,0 +1,10 @@
+echo ====================
+echo Installing multimedia codecs and stuff
+echo ====================
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
+
+echo ====================
+echo 'All done'
+echo ====================
